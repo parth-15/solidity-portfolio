dba2ea6b4f4691bcc11b45fa16d89f7b4fb571f4
Parth Patel <parth4321patel@gmail.com>
2022-09-25 22:37:38 +0530

    Initial setup

---
+++ lp/.eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    browser: false,
+    es2021: true,
+    mocha: true,
+    node: true,
+  },
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "standard",
+    "plugin:prettier/recommended",
+    "plugin:node/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  ignorePatterns: ['.eslintrc.js'],
+  parserOptions: {
+    ecmaVersion: 12,
+    project: './tsconfig.json'
+  },
+  rules: {
+    "node/no-unpublished-import": "off",
+    "node/no-missing-import": [
+      "error",
+      {
+        tryExtensions: [".ts", ".js", ".json", ".node"],
+      },
+    ],
+    "node/no-unsupported-features/es-syntax": [
+      "error",
+      { ignores: ["modules"] },
+    ],
+    '@typescript-eslint/no-floating-promises': ['error'],
+  },
+};
