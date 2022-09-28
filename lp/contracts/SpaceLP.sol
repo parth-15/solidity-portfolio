1f44af9b2e43d6e5a24b9e3bb80af451a40adbfa
Parth Patel <parth4321patel@gmail.com>
2022-09-28 19:42:32 +0530

    Initial draft contract

---
+++ lp/contracts/SpaceLP.sol
@@ -0,0 +1,188 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "./SpaceCoin.sol";
+
+contract SpaceLP is ERC20 {
+    uint256 public constant MINIMUM_LIQUIDITY = 1000;
+    SpaceCoin public immutable spaceCoin;
+    uint256 public spcTokenBalance;
+    uint256 public ethBalance;
+
+    constructor(SpaceCoin _spaceCoin) ERC20("LP TOKENS", "LPT") {
+        spaceCoin = _spaceCoin;
+    }
+
+    /// @notice Adds ETH-SPC liquidity to LP contract
+    /// @param to The address that will receive the LP tokens
+    function deposit(address to) external payable returns (uint256) {
+        uint256 spcTransferred = spaceCoin.balanceOf(address(this)) -
+            spcTokenBalance;
+        uint256 ethTransferred = address(this).balance - ethBalance;
+        uint256 lpTokensTotalSupply = totalSupply();
+        uint256 liquidity = 0;
+        if (lpTokensTotalSupply == 0) {
+            liquidity = sqrt(spcTransferred * ethTransferred);
+            require(liquidity > MINIMUM_LIQUIDITY, "insufficient liquidity");
+            _mint(to, liquidity - MINIMUM_LIQUIDITY);
+            _mint(address(0), MINIMUM_LIQUIDITY);
+        } else {
+            liquidity = min(
+                (lpTokensTotalSupply * spcTransferred) / spcTokenBalance,
+                (lpTokensTotalSupply * ethTransferred) / ethBalance
+            );
+            require(liquidity > 0, "insufficient liquidity");
+            _mint(to, liquidity);
+        }
+
+        spcTokenBalance = spaceCoin.balanceOf(address(this));
+        ethBalance = address(this).balance;
+
+        return liquidity;
+    }
+
+    /// @notice Returns ETH-SPC liquidity to liquidity provider
+    /// @param to The address that will receive the outbound token pair
+    //@audit-info how to use check effects interaction pattern here
+    function withdraw(address to) external returns (uint256, uint256) {
+        uint256 lpTokenBalance = balanceOf(address(this));
+        uint256 currentSpcBalance = spaceCoin.balanceOf(address(this));
+        uint256 currentEthBalance = address(this).balance;
+        uint256 lpTotalSupply = totalSupply();
+
+        uint256 spcTokenOut = (currentSpcBalance * lpTokenBalance) /
+            lpTotalSupply;
+        uint256 ethOut = (currentEthBalance * lpTokenBalance) / lpTotalSupply;
+
+        //Effects
+        //@audit-info should transfer before burning or not
+        _burn(address(this), lpTokenBalance);
+
+        spcTokenBalance = spcTokenBalance - spcTokenOut;
+        ethBalance = ethBalance - ethOut;
+
+        //Interactions
+        spaceCoin.transfer(to, spcTokenOut);
+        (bool success, ) = to.call{value: ethOut}("");
+        require(success, "external call failed");
+
+        return (spcTokenOut, ethOut);
+    }
+
+    /// @notice Swaps ETH for SPC, or SPC for ETH
+    /// @param to The address that will receive the outbound SPC or ETH
+    //@audit-info what should happen if someone sends one using self destruct
+    function swap(address to) external payable {
+        uint256 spcTransferred = spaceCoin.balanceOf(address(this)) -
+            spcTokenBalance;
+        uint256 ethTransferred = address(this).balance - ethBalance;
+        if (spcTransferred > 0 && ethTransferred > 0) {
+            require(true, "can't send both assets");
+        }
+        if (spcTransferred == 0 && ethTransferred == 0) {
+            require(true, "no assets sent");
+        }
+        if (spcTransferred > 0) {
+            uint256 spcToBeAdded = spcTransferred - (spcTransferred) / 100;
+            uint256 ethInReserve = (spcTokenBalance * ethBalance) /
+                (spcTokenBalance + spcToBeAdded);
+            uint256 ethToBeSent = ethBalance - ethInReserve;
+            uint256 finalEthBalance = address(this).balance - ethToBeSent;
+            uint256 finalSpcBalance = spaceCoin.balanceOf(address(this));
+            //Checks
+            require(
+                finalEthBalance * finalSpcBalance >
+                    spcTokenBalance * ethBalance,
+                "invalid swap"
+            );
+            //Effects
+            ethBalance = address(this).balance - ethToBeSent;
+            spcTokenBalance = spaceCoin.balanceOf(address(this));
+            //Interactions
+            (bool success, ) = to.call{value: ethToBeSent}("");
+            require(success, "external call failed");
+        } else {
+            uint256 ethToBeAdded = ethTransferred - (ethTransferred) / 100;
+            uint256 spcInReserve = (spcTokenBalance * ethBalance) /
+                (ethBalance + ethToBeAdded);
+            uint256 spcToBeSent = spcTokenBalance - spcInReserve;
+            uint256 finalEthBalance = address(this).balance;
+            uint256 finalSpcBalance = spaceCoin.balanceOf(address(this)) -
+                spcToBeSent;
+            //checks
+            require(
+                finalEthBalance * finalSpcBalance >
+                    spcTokenBalance * ethBalance,
+                "invalid swap"
+            );
+            //effects
+            spcTokenBalance =
+                spaceCoin.balanceOf(address(this)) -
+                spcTokenBalance;
+            ethBalance = address(this).balance;
+            //interactions
+            spaceCoin.transfer(to, spcToBeSent);
+        }
+    }
+
+    function quoteSwapPrice(uint256 ethAmount, uint256 spcAmount)
+        external
+        view
+        returns (uint256)
+    {
+        if (ethAmount > 0 && spcAmount > 0) {
+            require(true, "can't send both assets");
+        }
+        if (ethAmount == 0 && spcAmount == 0) {
+            require(true, "no assets sent");
+        }
+        if (spcAmount > 0) {
+            uint256 spcToBeAdded = spcAmount - (spcAmount) / 100;
+            uint256 ethInReserve = (spcTokenBalance * ethBalance) /
+                (spcTokenBalance + spcToBeAdded);
+            uint256 ethToBeSent = ethBalance - ethInReserve;
+            uint256 finalSpcBalance = spcTokenBalance + spcToBeAdded;
+            uint256 finalEthBalance = ethInReserve;
+
+            require(
+                finalSpcBalance * finalEthBalance >
+                    spcTokenBalance * ethBalance,
+                "invalid swap"
+            );
+
+            return ethToBeSent;
+        } else {
+            uint256 ethToBeAdded = ethAmount - (ethAmount) / 100;
+            uint256 spcInReserve = (spcTokenBalance * ethBalance) /
+                (ethBalance + ethToBeAdded);
+            uint256 spcToBeSent = spcTokenBalance - spcInReserve;
+            uint256 finalSpcBalance = spcInReserve;
+            uint256 finalEthBalance = ethBalance - ethToBeAdded;
+            require(
+                finalSpcBalance * finalEthBalance >
+                    spcTokenBalance * ethBalance,
+                "invalid swap"
+            );
+            return spcToBeSent;
+        }
+    }
+
+    function min(uint x, uint y) internal pure returns (uint z) {
+        z = x < y ? x : y;
+    }
+
+    // babylonian method (https://en.wikipedia.org/wiki/Methods_of_computing_square_roots#Babylonian_method)
+    function sqrt(uint y) internal pure returns (uint z) {
+        if (y > 3) {
+            z = y;
+            uint x = y / 2 + 1;
+            while (x < z) {
+                z = x;
+                x = (y / x + x) / 2;
+            }
+        } else if (y != 0) {
+            z = 1;
+        }
+    }
+}
---
+++ lp/contracts/SpaceRouter.sol
@@ -0,0 +1,90 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.9;
+
+import "./SpaceLP.sol";
+import "./SpaceCoin.sol";
+
+contract SpaceRouter {
+    SpaceLP public immutable spaceLP;
+    SpaceCoin public immutable spaceCoin;
+
+    constructor(SpaceLP _spaceLP, SpaceCoin _spaceCoin) {
+        spaceLP = _spaceLP;
+        spaceCoin = _spaceCoin;
+    }
+
+    /// @notice Provides ETH-SPC liquidity to LP contract
+    /// @param spc The amount of SPC to be deposited
+    //@audit-info what happen to transfer tax here. how much amount should I fetch
+    function addLiquidity(uint256 spc) external payable returns (uint256) {
+        spaceCoin.transferFrom(msg.sender, address(spaceLP), spc);
+        uint256 liquidity = spaceLP.deposit{value: msg.value}(msg.sender);
+        return liquidity;
+    }
+
+    /// @notice Removes ETH-SPC liquidity from LP contract
+    /// @param lpToken The amount of LP tokens being returned
+    function removeLiquidity(uint256 lpToken)
+        external
+        returns (uint256, uint256)
+    {
+        //@audit-info check if this syntax is correct
+        spaceLP.transferFrom(msg.sender, address(spaceLP), lpToken);
+        (uint256 spcOut, uint256 ethOut) = spaceLP.withdraw(msg.sender);
+        return (spcOut, ethOut);
+    }
+
+    /// @notice Swaps ETH for SPC in LP contract
+    /// @param spcOutMin The minimum acceptable amout of SPC to be received
+    //@audit-info check if this is post tax
+    //@audit-info follow check-effect-interactions here
+    function swapETHForSPC(uint256 spcOutMin)
+        external
+        payable
+        returns (uint256)
+    {
+        uint256 expectedSpcToBeSent = spaceLP.quoteSwapPrice(msg.value, 0);
+
+        uint256 spcToBeReceivedPostTax = expectedSpcToBeSent -
+            (
+                spaceCoin.isTransferTaxEnabled()
+                    ? (expectedSpcToBeSent * spaceCoin.TRANSFER_TAX_PERCENT()) /
+                        100
+                    : 0
+            );
+
+        require(
+            spcToBeReceivedPostTax >= spcOutMin,
+            "invalid amount to be received"
+        );
+
+        spaceLP.swap{value: msg.value}(msg.sender);
+
+        return spcToBeReceivedPostTax;
+    }
+
+    /// @notice Swaps SPC for ETH in LP contract
+    /// @param spcIn The amount of inbound SPC to be swapped
+    /// @param ethOutMin The minimum acceptable amount of ETH to be received
+    //@audit-info follow check effect interactions here
+    function swapSPCForETH(uint256 spcIn, uint256 ethOutMin)
+        external
+        returns (uint256)
+    {
+        uint256 spcInPostTax = spcIn -
+            (
+                spaceCoin.isTransferTaxEnabled()
+                    ? (spcIn * spaceCoin.TRANSFER_TAX_PERCENT()) / 100
+                    : 0
+            );
+        uint256 expectedEthToBeSent = spaceLP.quoteSwapPrice(0, spcInPostTax);
+        require(
+            expectedEthToBeSent >= ethOutMin,
+            "invalid amount to be received"
+        );
+
+        spaceCoin.transferFrom(msg.sender, address(spaceLP), spcIn);
+        spaceLP.swap(msg.sender);
+        return expectedEthToBeSent;
+    }
+}
